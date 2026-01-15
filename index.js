2f42bb64a02583d58ddd126fb017934c2d82ae74
Juho Kangas <juho.kangas@wolt.com>
2026-01-15 13:59:57 +0200

    Add get endpoint

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ import express from 'express'
 const app = express()
 const port = 3000
 
+app.get('/todos', (req, res) => {
+    res.send('Hello from the server!')
+})
+
 app.listen(port, () => {
     console.log(`Server running now in port ${port}!!!`)
 })
