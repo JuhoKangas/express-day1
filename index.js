0f2a321c49b501a9f185d094aa151fc76bcd948a
Juho Kangas <juho.kangas@wolt.com>
2026-01-15 15:29:12 +0200

    Add cors

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 import express from 'express'
+import cors from 'cors'
 const app = express()
 const port = 3000
 
 app.use(express.json())
+app.use(cors())
 
 let todoData = [
     {id: 1, text: "First todo", completed: false },
